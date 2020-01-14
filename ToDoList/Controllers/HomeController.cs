52c230bdb3bef78a8ae1f1c0d7d514a96ff28383
jordanHS <53791340+jordanHS@users.noreply.github.com>
2020-01-14 13:51:48 -0800

    Initial commit

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+
+      [HttpGet("/favorite_photos")]
+      public ActionResult FavoritePhotos()
+      {
+        return View();
+      }
+
+    }
+}
